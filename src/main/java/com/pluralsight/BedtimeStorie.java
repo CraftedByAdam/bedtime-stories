45e0c678bee69b68eb1e86334459b2f11b616a2c
Your Name <youremail@example.com>
2026-04-20 15:14:06 -0400

    I by mistake wrote my commit message with the initial commit:
    I successfully made the FileReader and BufferReader take in the external files and store them and then print the poem depending on the users choice.

--- src/main/java/com/pluralsight/BedtimeStorie.java
+++ src/main/java/com/pluralsight/BedtimeStorie.java
@@ -1,4 +1,31 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class BedtimeStorie {
+    public static void main(String[] args) {
+
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the name of the file to read: ");
+            String fileName = input.nextLine();
+
+            FileReader file = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(file);
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Oops, something went wrong.");
+
+        }
+    }
 }
